1807db077a6bd7e2c0a91b52fe8d395ce3d42888
Angellucy460 <angelliona38@gmail.com>
2025-06-24 15:07:14 +0300

    Fetch Lab

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(books => renderBooks(books));
 }
 
+// A helper function to render the book titles to the DOM
 function renderBooks(books) {
   const main = document.querySelector('main');
+
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.textContent = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+// Call the function to trigger fetch on load
+document.addEventListener('DOMContentLoaded', fetchBooks);
